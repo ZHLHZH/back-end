b83d4dc0807c43f8599cda9d030c4c40026bece4
hzh0 <2505333024@qq.com>
2021-12-11 20:09:46 +0800

    v0.1

---
+++ xyshw/src/test/java/com/example/xyshw/XyshwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xyshw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyshwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
