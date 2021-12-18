ce8f1b32aba4bdbc4f31f9b0e507efa2d4202d67
hzh0 <2505333024@qq.com>
2021-12-18 15:09:53 +0800

    跨域问题

--- xyshw/src/main/java/com/example/xyshw/controller/UserController.java
+++ xyshw/src/main/java/com/example/xyshw/controller/UserController.java
@@ -8,10 +8,7 @@ import com.example.xyshw.model.User;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -21,6 +18,7 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @PostMapping("/register")
     public String login(@RequestBody User user) {
         int code = 200;
